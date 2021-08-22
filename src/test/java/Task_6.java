c4112be1f1dfd5636e1f723e510f7dfdfe0bce83
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-08-22 23:28:40 +0300

    Task_6 added more tests

--- src/test/java/Task_6.java
+++ src/test/java/Task_6.java
@@ -4,15 +4,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 public class Task_6 {
+    WebDriver driver = null;
+
+    @BeforeTest
+    public void preconditions() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
 
     @Test
-     public void test() {
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
+     public void test1() {
     driver.get("https://www.saucedemo.com/");
     driver.findElement(By.id("user-name")).sendKeys("standard_user");
     driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
@@ -25,6 +32,35 @@ public class Task_6 {
     Assert.assertEquals(driver.findElement(By.className("inventory_item_name")).getText(),"Test.allTheThings() T-Shirt (Red)");
     Assert.assertEquals(driver.findElement(By.className("inventory_item_price")).getText(),"$15.99");
 
-    driver.quit();
+    }
+
+    @Test
+    public void test2 () {
+        driver.get("https://www.saucedemo.com/");
+        Actions act = new Actions(driver);
+        WebElement element = driver.findElement(By.cssSelector(".login_password"));
+        act.moveToElement(element).perform();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[2]/h4")).getText(),"Password for all users:");
+        Assert.assertEquals(driver.findElement(By.tagName("h4")).getText(),"Accepted usernames are:");
+    }
+
+    @Test
+    public void test3() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        Assert.assertEquals(driver.findElement(By.name("add-to-cart-sauce-labs-backpack")).getText(),"ADD TO CART");
+        Actions act = new Actions(driver);
+        act.moveToElement(driver.findElement(By.partialLinkText("Linked")));
+        act.moveToElement(driver.findElement(By.linkText("Twitter"))).click(driver.findElement(By.linkText("Twitter"))).build().perform();
+        Thread.sleep(2000);
+
+    }
+
+
+    @AfterTest
+    public void postconditions() {
+        driver.quit();
     }
 }
