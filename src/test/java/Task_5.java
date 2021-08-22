2841116071c9195f7b9fb91c60a7212c9855588f
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-08-23 00:55:58 +0300

    Task_5

--- src/test/java/Task_5.java
+++ src/test/java/Task_5.java
@@ -6,7 +6,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
