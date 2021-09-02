0659a084b4bb9da40f0bafb43a603f2b3a893b29
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-09-02 15:08:05 +0300

    Task_6

--- src/test/java/Task_6.java
+++ src/test/java/Task_6.java
@@ -19,44 +19,6 @@ public class Task_6 {
     }
 
     @Test
-    public void testLog() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        Actions act = new Actions(driver);
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div"));
-        act.moveToElement(element).click(element).build().perform();
-        driver.findElement(By.cssSelector("#add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)")).click();
-        driver.findElement(By.className("shopping_cart_link")).click();
-        Assert.assertEquals(driver.findElement(By.className("inventory_item_name")).getText(), "Test.allTheThings() T-Shirt (Red)");
-        Assert.assertEquals(driver.findElement(By.className("inventory_item_price")).getText(), "$15.99");
-    }
-
-    @Test
-    public void moveTo() {
-        driver.get("https://www.saucedemo.com/");
-        Actions act = new Actions(driver);
-        WebElement element = driver.findElement(By.cssSelector(".login_password"));
-        act.moveToElement(element).perform();
-        Assert.assertEquals(driver.findElement(By.xpath("//div[2]/h4")).getText(), "Password for all users:");
-        Assert.assertEquals(driver.findElement(By.tagName("h4")).getText(), "Accepted usernames are:");
-    }
-
-    @Test
-    public void moving() throws InterruptedException {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
-        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
-        Assert.assertEquals(driver.findElement(By.name("add-to-cart-sauce-labs-backpack")).getText(), "ADD TO CART");
-        Actions act = new Actions(driver);
-        act.moveToElement(driver.findElement(By.partialLinkText("Linked")));
-        act.moveToElement(driver.findElement(By.linkText("Twitter"))).click(driver.findElement(By.linkText("Twitter"))).build().perform();
-        Thread.sleep(2000);
-    }
-
-    @Test
     public void test1() {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -82,7 +44,7 @@ public class Task_6 {
     }
 
     @Test
-    public void test3() {
+    public void test3() throws InterruptedException {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
@@ -91,10 +53,12 @@ public class Task_6 {
         Actions act = new Actions(driver);
         act.moveToElement(driver.findElement(By.partialLinkText("Linked")));
         act.moveToElement(driver.findElement(By.linkText("Twitter"))).click(driver.findElement(By.linkText("Twitter"))).build().perform();
+        Thread.sleep(2000);
     }
 
     @AfterTest
     public void postconditions() {
+
         driver.quit();
     }
 }
