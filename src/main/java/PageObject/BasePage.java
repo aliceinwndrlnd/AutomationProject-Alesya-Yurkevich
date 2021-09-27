f318f7777ae44f402b657dbe4bb5d6e0d9c06807
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-09-27 20:05:10 +0300

    PageObject

--- src/main/java/PageObject/BasePage.java
+++ src/main/java/PageObject/BasePage.java
@@ -1,23 +1,26 @@
 package PageObject;
 
-import driver.DriverCreation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import properties.PropertyReader;
 
+import java.util.Properties;
 
 
 public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected Actions actions;
+    protected Properties properties;
 
     protected BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, 5);
         this.actions = new Actions(driver);
+        this.properties = PropertyReader.getProperties();
     }
 
     protected void enter(By element, CharSequence... charSequences) {
@@ -30,8 +33,11 @@ public class BasePage {
         driver.findElement(element).click();
     }
 
-    protected void open(String url) {
+    protected void open() {
+        driver.get(properties.getProperty("url"));
+    }
 
+    protected void open(String url) {
         driver.get(url);
     }
 
