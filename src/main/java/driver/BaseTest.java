4307f8650626104a701085ae78bcf9287d0cc890
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-09-28 18:58:50 +0300

    driver

--- src/main/java/driver/BaseTest.java
+++ src/main/java/driver/BaseTest.java
@@ -2,22 +2,18 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import testNg.Listener;
+
 import static driver.DriverCreation.getDriver;
-import static driver.DriverCreation.quitDriver;
 
-
+@Listeners(Listener.class)
 public class BaseTest {
     public static WebDriver driver;
 
     @BeforeTest
     public void setupDriver(ITestContext context) {
-
         driver = getDriver();
-    }
-    @AfterTest
-    public void closeDriver() {
-        quitDriver();
     }
 }
